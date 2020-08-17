06168d4f5a07d83d555ff377fe49b0b8346346c5
Oderschvank <240495108@qq.com>
2020-08-17 16:21:14 +0800

    FileOperator

--- core/FileOperator.py
+++ core/FileOperator.py
@@ -14,6 +14,9 @@ class FileOperator(object):
 
     def __init__(self):
         print("Welcome to Archivisit!")
+        print("Current working path {}".format(os.getcwd()))
+        #生成当前工作路径，默认情况是软件所在位置
+        self.workingDir = os.getcwd()
 
         self.conn = sqlite3.connect("backups/Archives.db")
         self.cur = self.conn.cursor()
@@ -24,20 +27,21 @@ class FileOperator(object):
         EXIF信息是一个字典，存入数据库之前需要序列化
         '''
         self.cur.execute('''CREATE TABLE IF NOT EXISTS PICTURES (
-        PATH    TEXT    PRIMARY KEY NOT NULL ,
+        PATH        TEXT    PRIMARY KEY NOT NULL ,
         FILENAME    TEXT    NOT NULL ,
-        EXIF    TEXT    NOT NULL ,
-        TAGS    TEXT    NOT NULL);''')
+        EXIF        TEXT    NOT NULL ,
+        THUMBNAIL   TEXT    NOT NULL ,
+        USERTAGS    TEXT    NOT NULL);''')
         print("Picture Library initialized successfully.")
         '''
         初始化用于管理音乐的表
         key-路径
         '''
         self.cur.execute('''CREATE TABLE IF NOT EXISTS MUSIC(
-        PATH    TEXT PRIMARY KEY NOT NULL ,
-        INFO    TEXT    NOT NULL ,
-        ALMBU   TEXT    NOT NULL ,
-        TAGS    TEXT    NOT NULL );''')
+        PATH        TEXT PRIMARY KEY NOT NULL ,
+        INFO        TEXT    NOT NULL ,
+        ALMBU       TEXT    NOT NULL ,
+        USERTAGS    TEXT    NOT NULL );''')
         print("Music Library initialized successfully.")
         '''
         初始化用于管理文档的表
@@ -49,53 +53,49 @@ class FileOperator(object):
         print("Document Library initialized successfully.")
 
         self.conn.commit()
-#        conn.close()
 
-        self.suffixName = ['Anything']
-        self.picFiles = ['JPGE', 'JPG', 'jpg', 'jpge', 'raw', 'tiff']
-        self.musicFiles = ['MP3', 'mp3']
-        self.docFiles = ['pdf', 'doc', 'docx']
+        self.picFiles = ['.JPGE', '.JPG', '.jpg', '.jpge', '.raw', '.tiff', '.NEF']
+        self.musicFiles = ['.MP3', '.mp3']
+        self.docFiles = ['.pdf', '.doc', '.docx']
         self.costumeFiles = []
 
-    def RedirectLibrary(self, libPath):
-        self.libPath = libPath
+    #重新定位Archivies数据库,同时生成备份
+    # def RedirectLibrary(self, libPath):
+    #     self.libPath = libPath
 
-    def costumeSearch(self, costumeSuffix):
+    #增加管理自定义类型文件
+    def costumeSearch(self):
         print("What kind of files do you wish Archivist to manage?")
-        self.costumeFiles.insert(costumeSuffix)
-        print("Now Archivist will serach files including {}.".format(self.suffixName))
+        costumeSuffix = input()
+        self.costumeFiles.append(costumeSuffix)
+        print("Now Archivist will serach files including {}.".format(self.costumeFiles))
 
-
+    #搜索目标路径的所有文件
     def SearchSelectedPath(self, path):
         for fileName in os.listdir(path):
             suffixName = os.path.splitext(fileName)[-1]
+            #搜索图片
+            #读取EXIF信息并生成缩略图保存在backups/thumbnail
             if suffixName in self.picFiles:
                 print("Found Picture {}".format(fileName))
-                EXIF = json.dump(tools.Utilities.RawToTiff(path, fileName))
-                sql = "INSERT INTO PICTURES (PATH ,FILENAME ,EXIF, TAGS) VALUES ({}, {}, {}, {})".format(path, EXIF, fileName, "NONE")
-                self.cur.execute(sql)
-                self.conn.commit()
-            elif suffixName in self.musicFiles:
+                EXIF = tools.Utilities.ReadExif(path, fileName)
+                self.cur.execute('''
+                
+                ''')
+            #搜索音乐
+            if suffixName in self.musicFiles:
                 print("Found Music {}".format(fileName))
-                self.cur.execute('''(
-
-                );''')
-            elif suffixName in self.docFiles:
+            if suffixName in self.docFiles:
                 print("Found Document {}".format(fileName))
-                self.cur.execute('''(
-
-                );''')
-            elif suffixName in self.costumeFiles:
+            if suffixName in self.costumeFiles:
                 print("Found costume type file {}".format(fileName))
-                self.cur.execute('''(
+            if os.path.isdir(path + '/' + fileName):
+                self.SearchSelectedPath(path + '/' + fileName)
 
-                );''')
-            if os.path.isdir(path + "\\" + fileName):
-                self.SearchSelectedPath(path + "\\" + fileName)
 
-    def SetTag(self, path, tag):
-        self.cur.execute('''(
-        );''')
+    # def SetTag(self, path, tag):
+    #     self.cur.execute('''(
+    #     );''')
 
     def __del__(self):
         self.conn.commit()
