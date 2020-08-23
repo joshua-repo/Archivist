d6278e2bc5936f4f638c9e2403e1245d9fbf7105
Oderschvank <240495108@qq.com>
2020-08-23 21:26:44 +0800

    Controller.py

--- core/Controller.py
+++ core/Controller.py
@@ -1,4 +1,15 @@
+import core.StartPage
+import core.MainPage
 
 class Controller:
     def __init__(self):
         pass
+
+    def showStartPage(self):
+        self.startPage = core.StartPage()
+
+
+    def showMainPage(self):
+        self.mainPage = core.MainPage()
+
+
