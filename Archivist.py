bd7f5e5d1e767409aae8e9d96ba3013c3810d3ea
Oderschvank <240495108@qq.com>
2020-08-23 18:58:20 +0800

    Archivist

--- Archivist.py
+++ Archivist.py
@@ -1,4 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QApplication
 import core.FileOperator
+import core.StartPage
 
-fileOperator = core.FileOperator.FileOperator()
-fileOperator.SearchSelectedPath('.\\tests')
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    startPage = core.StartPage.StartPage()
+    sys.exit(app.exec_())
+
+# fileOperator = core.FileOperator.FileOperator()
+# fileOperator.SearchSelectedPath('.\\tests')
