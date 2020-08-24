84a1c846d58db063ddb9264fcb9030bfda1d0039
Oderschvank <240495108@qq.com>
2020-08-24 12:04:00 +0800

    Controller.py

--- core/Controller.py
+++ core/Controller.py
@@ -1,15 +1,29 @@
+import os
+import sys
+from PyQt5 import QtCore, QtWidgets
 import core.StartPage
 import core.MainPage
 
-class Controller:
+class Controller():
+
+    curPath = os.getcwd()
+    libPathDefault = 'backups/Archives.db'
+
     def __init__(self):
         pass
 
-    def showStartPage(self):
-        self.startPage = core.StartPage()
+    def isInitialized(self):
+        if os.path.exists(self.libPathDefault):
+            return True
+        else:
+            False
 
+
+    def showStartPage(self):
+        self.startPage = core.StartPage.StartPage()
+        self.startPage.switch_window.connect(self.showMainPage)
+        self.startPage.show()
 
     def showMainPage(self):
-        self.mainPage = core.MainPage()
-
-
+        self.mainPage = core.MainPage.MainPage()
+        self.mainPage.show()
