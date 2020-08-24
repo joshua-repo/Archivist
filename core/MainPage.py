589d957577d00b7e52acec80365a1b0496129756
Oderschvank <240495108@qq.com>
2020-08-24 12:04:52 +0800

    MainPage.py

--- core/MainPage.py
+++ core/MainPage.py
@@ -1,16 +1,53 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp
+from PyQt5.QtWidgets import QWidget, QPushButton, QMessageBox, QDesktopWidget, QMainWindow, QMenu
+from PyQt5.QtWidgets import QAction, qApp
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QCoreApplication
 
-class MainPage(QWidget):
+class MainPage(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.resize(1800, 1000)
+        self.resize(1800, 950)
         self.center()
         self.setWindowTitle('Archivist')
         self.setWindowIcon(QIcon('./core/icon.png'))
-        self.show()
+
+        exitAct = QAction('&Exit',self)
+        exitAct.triggered.connect(qApp.quit)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        tagMenu = menubar.addMenu('&Tags')
+        netMenu = menubar.addMenu('&NetDisk')
+        exitMenu = menubar.addMenu('&Exit')
+
+        importLib = QAction('Import the library', self)
+        exportLib = QAction('Export the library', self)
+
+        addLibMenu = QMenu('Add a new path to library', self)
+        addLocal = QAction('Local Path', self)
+        addURL = QAction('From URL', self)
+        addLibMenu.addAction(addLocal)
+        addLibMenu.addAction(addURL)
+
+        fileMenu.addMenu(addLibMenu)
+        fileMenu.addAction(importLib)
+        fileMenu.addAction(exportLib)
+
+        exitMenu.addAction(exitAct)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', "Are you sure to quit?",
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
