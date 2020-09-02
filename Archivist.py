4d9499f01cf0ac856e95981861e7e8f6af3430b6
Oderschvank <240495108@qq.com>
2020-09-02 22:10:40 +0800

    Archivist.py

--- Archivist.py
+++ Archivist.py
@@ -1,5 +1,4 @@
 import sys
-#import qdarkstyle
 from PyQt5.QtWidgets import QApplication
 import core.Controller
 import core.FileOperator
@@ -9,7 +8,6 @@ import core.MainPage
 
 def main():
     app = QApplication(sys.argv)
-    #app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     controller = core.Controller.Controller()
     if controller.isInitialized():
         controller.showMainPage()
@@ -17,10 +15,5 @@ def main():
         controller.showStartPage()
     sys.exit(app.exec_())
 
-def fileOperatorTest():
-    fileOperator = core.FileOperator.FileOperator()
-    fileOperator.SearchSelectedPath('./tests')
-
 if __name__ == '__main__':
     main()
-    #fileOperatorTest()
