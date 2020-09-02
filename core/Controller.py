3f26599ca6305ecd42bd3bbadb92ca0783daad79
Oderschvank <240495108@qq.com>
2020-09-02 22:11:07 +0800

    Controller.py

--- core/Controller.py
+++ core/Controller.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import core.FileOperator
 from PyQt5 import QtCore, QtWidgets
 import core.StartPage
 import core.MainPage
@@ -18,12 +19,32 @@ class Controller():
         else:
             False
 
-
     def showStartPage(self):
         self.startPage = core.StartPage.StartPage()
-        self.startPage.switch_window.connect(self.showMainPage)
+        self.startPage.switchWindow.connect(self.showMainPage) #将switch_window这个信号和showMainPage这个函数连接
         self.startPage.show()
 
-    def showMainPage(self):
-        self.mainPage = core.MainPage.MainPage()
-        self.mainPage.show()
+    # def showMainPage(self):
+    #     #这个是给Controller下isInitialized调用的
+    #     #如果已经初始化完毕，那么将会在Controller里调用一个无参数的showMainPage
+    #     self.mainPage = core.MainPage.MainPage()
+    #     self.mainPage.show()
+
+    # def showMainPage(self, libPath): #接受一个参数的函数，带有libPath
+    #     self.mainPage = core.MainPage.MainPage()
+    #     self.mainPage.initializeLib(libPath)
+    #     self.mainPage.show()
+
+    def showMainPage(self, *args): #Python的多态是这么实现的
+        if args[0] == "": #此时传入的路径是空的，初始化数据库
+            self.mainPage = core.MainPage.MainPage()
+            self.mainPage.createDB()
+            self.mainPage.show()
+        else:
+            self.mainPage = core.MainPage.MainPage()
+            self.mainPage.readDB()
+            self.mainPage.show()
+
+
+    def __del__(self):
+        print("Controller is dead.")
