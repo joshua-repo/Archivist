7ab70a1c33af7d98ba67d6bfcb3db8d1f98a1c63
Oderschvank <240495108@qq.com>
2020-09-11 09:38:17 +0800

    MainPage.py

--- component/MainPage.py
+++ component/MainPage.py
@@ -1,17 +1,19 @@
-import os
-import sqlite3
-from PyQt5.QtSql import QSqlDatabase, QSqlQuery
-from PyQt5.QtWidgets import QWidget, QPushButton, QMessageBox, QDesktopWidget, QMainWindow, QMenu, QTabBar, QTabWidget, \
-    QFileDialog, QGridLayout, QSplitter, QHBoxLayout, QFrame, QAction, QGroupBox, QVBoxLayout, QTableView
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import Qt
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5.QtCore import QPoint, Qt
+
+import component.ContentTabView
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlQueryModel
+from PyQt5.QtWidgets import QWidget, QMessageBox, QDesktopWidget, QMainWindow, QMenu, QFileDialog, QGridLayout, QAction, \
+    QTableView, QListView, QLabel, QLineEdit
+from PyQt5.QtGui import QIcon, QCursor
+
 
 class MainPage(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.localPath = QFileDialog.getExistingDirectory(self, 'Select the directory', '/')
+        self.initDB()
         self.initUI()
+        self.displayLocation()
 
     def initUI(self):
         self.menuBarInit()
@@ -20,7 +22,7 @@ class MainPage(QMainWindow):
         self.resize(1800, 950)
         self.center()
         self.setWindowTitle('Archivist')
-        self.setWindowIcon(QIcon('./core/icon.png'))
+        self.setWindowIcon(QIcon('./resource/icon.png'))
 
     def menuBarInit(self):
         menubar = self.menuBar()
@@ -40,17 +42,28 @@ class MainPage(QMainWindow):
         addLibMenu.addAction(addURL)
         addLibMenu.addAction(addDisk)
 
+        addFile = QAction('Add a new file to library', self)
+        addNewType = QAction('Add a new file type', self)
+
+        searchFile = QAction('Search File', self)
+        searchFile.triggered.connect(self.searchPage)
+
         importLib = QAction('Import the library', self)
         exportLib = QAction('Export the library', self)
 
         fileMenu.addMenu(addLibMenu)
+        fileMenu.addAction(addFile)
+        fileMenu.addAction(addNewType)
+        fileMenu.addAction(searchFile)
         fileMenu.addAction(importLib)
         fileMenu.addAction(exportLib)
 
         #editMenu
-        addFileType = QAction('Add a new file type', self)
+        selectAll = QAction('Select All', self)
+        preference = QAction('Preference', self)
 
-        editMenu.addAction(addFileType)
+        editMenu.addAction(selectAll)
+        editMenu.addAction(preference)
 
         #viewMenu
         iconSizeMenu = QMenu('Icon size', self)
@@ -67,11 +80,15 @@ class MainPage(QMainWindow):
         typeOrder = QAction('Type', self)
         dataOrder = QAction('Data', self)
         tagsOrder = QAction('Tags', self)
+        ratingOrder = QAction('Rating', self)
+        keywordsOrder = QAction('Keywords', self)
         sortMenu.addAction(nameOrder)
         sortMenu.addAction(sizeOrder)
         sortMenu.addAction(typeOrder)
         sortMenu.addAction(dataOrder)
         sortMenu.addAction(tagsOrder)
+        sortMenu.addAction(ratingOrder)
+        sortMenu.addAction(keywordsOrder)
 
         viewMenu.addMenu(iconSizeMenu)
         viewMenu.addMenu(sortMenu)
@@ -79,55 +96,92 @@ class MainPage(QMainWindow):
         #tagsMenu
         addTags = QAction('New Label', self)
         addRating = QAction('New Rating', self)
+        addKeyword = QAction('New Keyword', self)
 
         tagsMenu.addAction(addTags)
         tagsMenu.addAction(addRating)
+        tagsMenu.addAction(addKeyword)
 
     def centralWidgetGridLayout(self):
-        self.testBottom1 = QPushButton('test1', self)
-        self.testBottom2 = QPushButton('test2', self)
+        self.locationLabel = QLabel('Locations')
+        self.filterLabel = QLabel('Filter')
+        self.metadataLabel = QLabel('Metadata')
+        self.previewLabel = QLabel('Preview')
+
+        self.locationView = QListView()
+        self.locationView.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.locationView.customContextMenuRequested(QPoint).connect(self.locationPopMenu)
+        self.filterView = QTableView()
+        self.mainView = component.ContentTabView.contentTabView()
+        self.previewView = QTableView()
+        self.metadateView = QListView()
 
         self.grid = QGridLayout()
-        self.grid.addWidget(self.testBottom1, 0, 0)
-        self.grid.addWidget(self.testBottom2, 0, 1)
+        self.grid.addWidget(self.locationLabel, 0, 0)
+        self.grid.addWidget(self.locationView, 1, 0)
+        self.grid.addWidget(self.filterLabel, 2, 0)
+        self.grid.addWidget(self.filterView, 3, 0)
+        # addWidget(self, QWidget, row, column, rowSpan, columnSpan) 可以被这样重载
+        # rowSpan, columnSpan 代表跨行，跨列
+        # 参数-1代表直接将view延伸至底部
+        self.grid.addWidget(self.mainView, 0, 1, -1, 1)
+        self.grid.addWidget(self.previewLabel, 0, 2)
+        self.grid.addWidget(self.previewView, 1, 2, 1, 2)
+        self.grid.addWidget(self.metadataLabel, 2, 2)
+        self.grid.addWidget(self.metadateView, 3, 2)
 
         self.layoutWidget = QWidget()
         self.layoutWidget.setLayout(self.grid)
         self.setCentralWidget(self.layoutWidget)
 
+    def initDB(self):
+        # 建立一个全局的连接
+        self.db = QSqlDatabase.addDatabase('QSQLITE')
+        self.db.setDatabaseName('./core/Archives.db')
+        self.db.open()
+        self.query = QSqlQuery()
+
+    def displayLocation(self):
+        # 刷新location显示页面
+        self.query.prepare("SELECT * FROM LIBINFO")
+        self.query.exec()
+        self.model = QSqlQueryModel()
+        self.model.setQuery(self.query)
+        self.locationView.setModel(self.model)
 
     def addLocalPath(self):
-        db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('./backups/Archives.db')
-        db.open()
-        query = QSqlQuery()
-        query.exec("INSERT INTO LIBINFO (LOCATIONS) values ('{}')".format(self.localPath))
-        db.close()
+        localPath = QFileDialog.getExistingDirectory(self, 'Select the directory', '/')
+        self.query.exec("INSERT INTO LIBINFO (LOCATIONS) values ('{}')".format(localPath))
+        self.displayLocation()
+
+    def locationPopMenu(self, point):
+        popMenu = QMenu()
+        removeAct = QAction('Remove', self)
+        #popMenu.addAction(removeAct)
+        if self.locationView.itemAt(point):
+            popMenu.addAction(removeAct)
+        popMenu.exec_(self.locationView.mapToGlobal(point))
 
     def createDB(self):
-        db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('./backups/Archives.db')
-        db.open()
-        query = QSqlQuery()
-        query.exec('''CREATE TABLE IF NOT EXISTS LIBINFO(
+        self.query.exec('''CREATE TABLE IF NOT EXISTS LIBINFO(
                 LOCATIONS   TEXT    NOT NULL    UNIQUE 
             );''')
 
-        query.exec('''CREATE TABLE IF NOT EXISTS PICTURES(
+        self.query.exec('''CREATE TABLE IF NOT EXISTS PICTURES(
                 PATH        TEXT    PRIMARY KEY NOT NULL UNIQUE ,
                 FILENAME    TEXT    NOT NULL ,
                 EXIF        TEXT    NOT NULL ,
                 USERTAGS    TEXT    NOT NULL
             );''')
 
-        query.exec('''CREATE TABLE IF NOT EXISTS PDFDOC(
+        self.query.exec('''CREATE TABLE IF NOT EXISTS PDFDOC(
                 PATH        TEXT    PRIMARY KEY UNIQUE ,
                 FILENAME    TEXT    NOT NULL ,
                 ARRAGE      TEXT    NOT NULL ,
                 USERTAGS    TEXT    NOT NULL
             );''')
 
-        query.exec('''CREATE TABLE IF NOT EXISTS MUSIC(
+        self.query.exec('''CREATE TABLE IF NOT EXISTS MUSIC(
                 PATH        TEXT    PRIMARY KEY UNIQUE ,
                 FILENAME    TEXT    NOT NULL ,
                 METADATA    TEXT    NOT NULL ,
@@ -136,10 +190,12 @@ class MainPage(QMainWindow):
                 USERTAGS    TEXT    NOT NULL ,
                 STYLE       TEXT    NOT NULL
             );''')
-        db.close()
 
     def readDB(self):
         #当用户需要从外部导入Archives.db时调用
+        pass
+
+    def searchPage(self):
         pass
 
     def center(self):
@@ -155,3 +211,6 @@ class MainPage(QMainWindow):
             event.accept()
         else:
             event.ignore()
+
+    def __del__(self):
+        self.db.close()
