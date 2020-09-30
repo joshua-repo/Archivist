e2c26e561fcd4b6fc22b0f16644fdc7e30da6af0
Oderschvank <240495108@qq.com>
2020-09-30 09:52:13 +0800

    core

--- core/FileOperator.py
+++ core/FileOperator.py
@@ -29,8 +29,53 @@ def searchPath(path, query):
 def sortFiles(query):
     pass
 
-def ReadExif(path, filename):
-    f = open(path+ "\\" + filename, 'rb')
+def ReadExif(path):
+    f = open(path, 'rb')
     tags = exifread.process_file(f)
     f.close()
     return tags
+
+def unionQuery(query, tagsList, ratingsList, keywordsList):
+    sql = ""
+    if tagsList == []:
+        pass
+    elif len(tagsList) == 1:
+        sql = "SELECT FILENAME FROM FileLibrary WHERE USERTAGS == '{}'".format(tagsList[0])
+    else:
+        sql = "SELECT FILENAME FROM FileLibrary WHERE USERTAGS == '{}'".format(tagsList[0])
+        for i in range(1, len(tagsList)):
+            print(tagsList[i])
+            sql += "UNION SELECT FILENAME FROM FileLibrary WHERE USERTAGS == '{}'".format(tagsList[i])
+
+    if ratingsList == []:
+        pass
+    elif len(ratingsList) == 1:
+        if sql == "":
+            sql = "SELECT FILENAME FROM FileLibrary WHERE RATING == '{}'".format(ratingsList[0])
+        else:
+            sql += "UNION SELECT FILENAME FROM FileLibrary WHERE RATING == '{}'".format(ratingsList[0])
+    else:
+        if sql == "":
+            sql = "SELECT FILENAME FROM FileLibrary WHERE RATING == '{}'".format(ratingsList[0])
+            for i in range(1, len(ratingsList)):
+                sql += "UNION SELECT FILENAME FROM FileLibrary WHERE RATING == '{}'".format(ratingsList[i])
+        else:
+            for i in range(0, len(ratingsList)):
+                sql += "UNION SELECT FILENAME FROM FileLibrary WHERE RATING == '{}'".format(ratingsList[i])
+
+    if keywordsList == []:
+        pass
+    elif len(keywordsList) == 1:
+        if sql == "":
+            sql = "SELECT FILENAME FROM FileLibrary WHERE KEYWORDS == '{}'".format(keywordsList[0])
+        else:
+            sql += "UNION SELECT FILENAME FROM FileLibrary WHERE KEYWORDS == '{}'".format(keywordsList[0])
+    else:
+        if sql == "":
+            sql = "SELECT FILENAME FROM FileLibrary WHERE KEYWORDS == '{}'".format(keywordsList[0])
+            for i in range(1, len(keywordsList)):
+                sql += "UNION SELECT FILENAME FROM FileLibrary WHERE KEYWORDS == '{}'".format(keywordsList[i])
+        else:
+            for i in range (0, len(ratingsList)):
+                sql += "UNION SELECT FILENAME FROM FileLibrary WHERE KEYWORDS == '{}'".format(keywordsList[i])
+    query.exec(sql)
